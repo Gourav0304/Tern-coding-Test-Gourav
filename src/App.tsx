464c3e2d3fbd70861c5388435667df254a495521
Gourav0304 <gouravrathore0304@gmail.com>
2025-11-24 15:48:23 +0530

    feat: configure react-router and create initial Home, Video, and Gif components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-const App = () => {
-  return <>test</>;
-};
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home/Home';
+import Video from './pages/Video/Video';
+import Gif from './pages/Gif/Gif';
+import Layout from './components/Layout/Layout';
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/gif" element={<Gif />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Gif/Gif.tsx
@@ -0,0 +1,7 @@
+export default function Gif() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">GIF Page</h1>
+    </div>
+  );
+}
